6024d9b9f90bd7edad49f4b834f903eeff7e306c
34383c <34383c@users.noreply.github.com>
2016-02-17 22:55:24 +0000

    Some improvements.

--- examples.jl
+++ examples.jl
@@ -1,10 +1,17 @@
 using hrr
 
+# would a macro be better?
+# print vectors
+function printv{T <: Real}(x::AbstractVector{T})
+    println(summary(x))
+    println("    $(join(length(x) > 7 ? [x[1:3];"...";x[end-2:end]] : x, "  "))")
+end
+
 println("####### START OF EXAMPLES #######\n")
 
 # array of pairs of examples? (to easily enumerate them?)
 println("Example 1.  Given a vector x = [1, 2, 3, 4, 5], the involution of x ",
         "should return the vector x_bar = [1, 5, 4, 3, 2].")
-hrr.invol([1, 2, 3, 4, 5], debug=true)
+printv(hrr.invol([1, 2, 3, 4, 5]))
 
 println("\n######## END OF EXAMPLES ########")
--- hrr.jl
+++ hrr.jl
@@ -1,29 +1,14 @@
 module hrr
 
-# TODO: ::Vector{T} (or ::AbstractVector{T} (whats the difference?)) instead of
-#       ::Array{Float64, 1}, where T <: Real? (If implem for diff types are too
-#       diff, consider methods?)
-# TODO: throw argument errors instead of asserts?
-# TODO: consider removing the debug functionality?
-
-# move to another file?
-# would a macro be better?
-# print vectors
-function printv(x::Vector)
-    println(summary(x))
-    println("    $(join(length(x) > 7 ? [x[1:3];"...";x[end-2:end]] : x, "  "))")
-end
+# TODO: introduce in-place methods?
 
 export cconv, vadd, invol
 
 # BINDING -- circular convolution of two vectors with the same dimensions
-function cconv(x::Array{Float64, 1}, y::Array{Float64, 1}; debug=false)
-    @assert(length(x) > 0 && length(y) > 0, "the length of the vectors must be greater than zero")
-    @assert(length(x) == length(y), "the vectors must be of the same dimensions")
-    if debug
-        println("x = $x")
-        println("y = $y")
-    end
+function cconv{T <: Real}(x::AbstractVector{T}, y::AbstractVector{T})
+    (isempty(x) || isempty(y)) && throw(ArgumentError("arguments must not be empty"))
+    (length(x) != length(y)) && throw(ArgumentError("dimensions must match"))
+    # TODO: result and term need to be of the correct type...
     result = Array{Float64, 1}(length(x))
     for i in 1:length(result)
         term = Float64(0)
@@ -32,34 +17,19 @@ function cconv(x::Array{Float64, 1}, y::Array{Float64, 1}; debug=false)
         end
         result[i] = term
     end
-    if debug
-        println("x*y = $result")
-    end
     result
 end
 
-# TODO: ::Vector instead of ::Array{Float64, 1}? (Need to make sure cconv is valid for any type of vector)
 # SUPERPOSITION -- vector addition
-function vadd(x::Array{Float64, 1}, y::Array{Float64, 1}; debug=false)
-    isempty(x) && throw(ArgumentError("argument must not be empty"))
-    if debug
-        println("x = $x")
-        println("y = $y")
-    end
+function vadd{T <: Real}(x::AbstractVector{T}, y::AbstractVector{T})
+    (isempty(x) || isempty(y)) && throw(ArgumentError("arguments must not be empty"))
     x+y
 end
 
 # UNBINDING -- involution of a vector (approximate inverse of a vector with respect to circular convolution).
-function invol(x::Vector; debug=false)
+function invol{T <: Real}(x::AbstractVector{T})
     isempty(x) && throw(ArgumentError("argument must not be empty"))
-    if debug
-        printv(x)
-    end
-    result = [x[mod(-i + 1, 5) + 1] for i in 1:length(x)]
-    if debug
-        printv(result)
-    end
-    result
+    [x[mod(-i + 1, 5) + 1] for i in 1:length(x)]
 end
 
 end # module hrr
