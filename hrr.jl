7f8325f033f5a070822b1adfcd02caeb2ce7498a
34383c <34383c@users.noreply.github.com>
2016-02-22 15:47:35 +0000

    Started implementing an associative memory.

--- hrr.jl
+++ hrr.jl
@@ -1,8 +1,13 @@
 module hrr
 
 # TODO: introduce in-place methods?
+# TODO: doc-strings?
 
 export cconv, vadd, invol
+
+type AssociativeMemory <: Tuple
+    #am::Tuple{Tuple{AbstractVector, AbstractVector}}
+end
 
 # BINDING -- circular convolution of two vectors with the same dimensions
 function cconv{T <: Real}(x::AbstractVector{T}, y::AbstractVector{T})
@@ -40,20 +45,35 @@ function invol{T <: Real}(x::AbstractVector{T})
     return [x[mod(-i + 1, length(x)) + 1] for i in 1:length(x)]
 end
 
-function getIDvector(dimension::Int64 = 512)
+function getIDvector(dimensions::Integer = 512)
     # Return a uniformly distributed (see Note 1) random vector from the
-    # D-dimensional unit hypersphere (See Note 2).
+    # D-dimensional unit hypersphere (see Note 2).
     #
-    # Note 1: we use the fact that a normalised D-dimensional random Gaussian
-    # (i.e. normally-distributed) vector is uniformly distributed over the
-    # D-dimensional unit hypersphere.
+    # Note 1: we use the fact that a D-dimensional random Gaussian
+    # (i.e. normally-distributed) vector, when normalised, is uniformly
+    # distributed over the D-dimensional unit hypersphere.
     #
     # Note 2: A vector from the D-dimensional unit hypersphere is a unit
     # D-dimensional vector, i.e. a D-dimensional vector with vector norm
     # (i.e. L-2 norm) equal to 1.
 
-    random_gaussian_vector = randn(dimension)
+    random_gaussian_vector = randn(dimensions)
     return random_gaussian_vector / norm(random_gaussian_vector)
+end
+
+const threshold = 0.3
+function associate{T <: Real}(x::AbstractVector{T}, assoc_mem::AssociativeMemory)
+    if isempty(x)
+        throw(ArgumentError("argument must not be empty"))
+    end
+
+    sum = zeros(x)
+    for i in 1:length(associative_memory)
+        similarity = dot(associative_memory[i,1], x)
+        scale = (similarity > threshold) ? 1 : 0
+        sum += scale * associative_memory[i,2]
+    end
+    return sum
 end
 
 end # module hrr
